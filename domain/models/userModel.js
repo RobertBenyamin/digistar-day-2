186561ac8f4183462bb75fec9b1d4a03f5c015b1
RobertBenyamin <robertbenyamino@gmail.com>
2024-09-10 21:24:59 +0700

    refactor: Update user model to hash password using bcrypt

--- domain/models/userModel.js
+++ domain/models/userModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     userId: {
         type: String,
         default: () => crypto.randomUUID(),
-        unique: true,
+        unique: true
     },
     name: {
         type: String,
@@ -15,6 +16,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        this.password = await bcrypt.hash(this.password, 10); 
+    }
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
